90129baa5749c0d133d28c286be9c53e5a9b1dbb
Luisfelipedev27 <luisfelipe1582@hotmail.com>
2025-07-08 01:40:46 -0300

    setup endpoint authetication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::API
+  helper_method :current_patient
+
+  private
+
+  def authenticate_patient
+    api_key = request.headers['Authorization']&.gsub('Bearer ', '')
+    @current_patient = Patient.find_by(api_key: api_key) if api_key.present?
+
+    unless @current_patient
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
+  end
+
+  def current_patient
+    @current_patient
+  end
 end
