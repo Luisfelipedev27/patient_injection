dfcb2bc6b77c3fbbf2ca4ccc012c5ac0bec373fd
Luisfelipedev27 <luisfelipe1582@hotmail.com>
2025-07-08 01:55:13 -0300

    create patient model with validations and api_key for authentication

---
+++ app/models/patient.rb
@@ -0,0 +1,14 @@
+class Patient < ApplicationRecord
+  has_many :injections, dependent: :destroy
+
+  validates :api_key, presence: true, uniqueness: true
+  validates :treatment_schedule_days, presence: true, numericality: { greater_than: 0 }
+
+  before_validation :generate_api_key, on: :create
+
+  private
+
+  def generate_api_key
+    self.api_key = SecureRandom.hex(16)
+  end
+end
